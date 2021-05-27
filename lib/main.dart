db360708e12cf99cf891408a406d568a17399df4
Alestry Pérez <alestry.perez@hyperisland.se>
2021-05-28 01:00:27 +0200

    added asset

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
+import 'package:flutter/services.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
 
 void main() => runApp(MyApp());
@@ -8,63 +11,53 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter AR',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter AR'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  MyHomePageState createState() => MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class MyHomePageState extends State<MyHomePage> {
   ArCoreController arCoreController;
-
-  _onArCoreViewCreated(ArCoreController _arcoreController) {
-    arCoreController = _arcoreController;
-    _addCube(arCoreController);
-  }
-
-  void _addCube(ArCoreController controller) {
-  final material = ArCoreMaterial(
-    color: Colors.blue,
-    metallic: 1.0,
-  );
-  final cube = ArCoreCube(
-    materials: [material],
-    size: vector.Vector3(0.2, 0.2, 0.2),
-    
-  );
-  final node = ArCoreRotatingNode(
-    shape: cube,
-    position: vector.Vector3(0, 0, -1.5),
-    rotation: vector.Vector4(0,0,0,0),
-  );
-  
-  
-  controller.add(node);
-}
-
-  @override
-  void dispose() {
-    arCoreController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ArCoreView(
         onArCoreViewCreated: _onArCoreViewCreated,
+        enableTapRecognizer: true,
       ),
     );
+  }
+
+  void _onArCoreViewCreated(ArCoreController _arcoreController) {
+    arCoreController = _arcoreController;
+    addCube(arCoreController);
+    //sceneSetting(arCoreController);
+  }
+
+  void sceneSetting(ArCoreView view) {
+    
+  }
+
+  void addCube(ArCoreController controller) {
+    final material = ArCoreMaterial(
+      color: Colors.blue,
+      metallic: 1.0,
+    );
+    final cube = ArCoreCube(
+      materials: [material],
+      size: vector.Vector3(0.4, 0.4, 0.4),
+    );
+    final node = ArCoreRotatingNode(
+      shape: cube,
+      position: vector.Vector3(0, 0, -1.0),
+      rotation: vector.Vector4(0, 0, 0, 0),
+    );
+    controller.add(node);
   }
 }
